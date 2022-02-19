cfd91c15efd3df310997ac1507030c2f410e6e5c
Andriy Halelyuka <81869325+julia-lazariv@users.noreply.github.com>
2022-02-19 16:13:23 +0200

    add Lab4.1

---
+++ Lab4.1/Lab4.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lab4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double P = 0.0;
+            Console.Write("k = ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            Console.Write("N = ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            while (k <= N)
+            {
+                P = (k * k / N * N) * Math.Cos(N * N / k * k);
+                Console.WriteLine("P = " + P);
+                return;
+            }
+            do
+            {
+                P = (k * k / N * N) * Math.Cos(N * N / k * k);
+                Console.WriteLine("P = " + P);
+            } while (k <= N);
+            for (k = 1; k <= N; k++)
+            {
+                P = (k * k / N * N) * Math.Cos(N * N / k * k);
+                Console.WriteLine("P = " + P);
+            }
+            for (k = 1; k >= N; k--)
+            {
+                P = (k * k / N * N) * Math.Cos(N * N / k * k);
+                Console.WriteLine("P = " + P);
+            }
+        }
+    }
+}
