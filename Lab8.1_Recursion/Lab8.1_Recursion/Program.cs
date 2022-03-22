6a3131d054b09bab0eb1e757548b44c2256186aa
Andriy Halelyuka <81869325+julia-lazariv@users.noreply.github.com>
2022-03-22 20:16:00 +0200

    add Lab8.1_Recursion

---
+++ Lab8.1_Recursion/Lab8.1_Recursion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab8._1_Recursion
+{
+    class Program
+    {
+        static bool Search(string str, int i)
+        {
+            if (str.Length < 3)
+                return false;
+            if (i >= str.Length)
+                return false;
+            if (str.Substring(i, 1) == "*" &&
+                str.Substring(i + 1, 1) == "*" &&
+                str.Substring(i + 2, 1) == "*")
+                return true;
+            return Search(str, i + 1);
+        }
+        static String Change(string s)
+        {
+            String t = s.Replace("***", "!!");
+            return t;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("str = ");
+            string str = Console.ReadLine();
+            Console.WriteLine("String contained groups of '***' - " + Search(str, 1));
+            Console.WriteLine("Modified string  = " + Change(str));
+        }
+    }
+}
