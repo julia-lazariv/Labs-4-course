711b6d95a235e5f048ca4172673a6a0aa478ea39
Andriy Halelyuka <81869325+julia-lazariv@users.noreply.github.com>
2022-04-10 17:15:47 +0300

    add Lab4

---
+++ Lab4/Lab4/Fraction.cs
@@ -0,0 +1,58 @@
+﻿// Fraction.cs
+using System;
+
+namespace Lab4
+{
+    class Fraction
+    {
+        private int first;
+        private int second;
+
+        public int GetFirst() { return first; }
+        public int GetSecond() { return second; }
+        public void setFirst(int first) { this.first = first; }
+
+        public bool SetSecond(int s)
+        {
+            if (s > 0)
+            {
+                second = s;
+                return true;
+            }
+            else
+            {
+                second = 0;
+                return false;
+            }
+        }
+        public void Init(int first, int second)
+        {
+            setFirst(first);
+            SetSecond(second);
+        }
+        public void Read()
+        {
+            Console.Write("first: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.Write("second: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Init(first, second);
+        }
+        public void Display()
+        {
+            Console.WriteLine();
+            Console.WriteLine("First: " + first);
+            Console.WriteLine("Second: " + second);
+            Console.WriteLine();
+        }
+        // обчислення значення дробу
+        public double GetValue() { return first / second; }
+        // скорочення дробу
+        public int Contraction(int a, int b)
+        {
+            while (b != 0)
+                b = a % (a = b);
+            return a;
+        }
+    }
+}
---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Program.cs
+using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction g = new Fraction();
+            g.Read();
+            g.Display();
+            // обчислення значення дробу
+            Console.WriteLine("Fraction value: " + g.GetValue());
+            Console.WriteLine();
+            // скорочення дробу
+            int del = g.Contraction(g.GetFirst(), g.GetSecond());
+            Console.WriteLine("first = " + g.GetFirst() / del);
+            Console.WriteLine("second = " + g.GetSecond() / del);
+        }
+    }
+}
